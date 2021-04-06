ea6feaf86650b05a47205235851a5d000fc5f29b
oliver zhang <ozha8307@uni.sydney.edu.au>
2021-04-06 12:00:00 +1000

    check boxes against solid tiles

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -45,3 +45,14 @@ public static class Level
                     return new Vector2(c + 0.5f, TileBottom(r));
         return new Vector2(W * 0.5f, H * 0.5f);
     }
+
+    /// True if a solid tile overlaps this world-space box.
+    public static bool OverlapsSolid(Rect b)
+    {
+        const float e = 1e-4f;
+        for (int r = RowAt(b.yMax - e); r <= RowAt(b.yMin + e); r++)
+            for (int c = ColAt(b.xMin + e); c <= ColAt(b.xMax - e); c++)
+                if (Solid(c, r)) return true;
+        return false;
+    }
+}
