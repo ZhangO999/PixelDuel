a386093fe3f4948e6d43596784f69276af7b621e
oliver zhang <ozha8307@uni.sydney.edu.au>
2021-07-13 12:00:00 +1000

    generate placeholder sprites in code

--- Assets/Scripts/Art.cs
+++ Assets/Scripts/Art.cs
@@ -10,3 +10,19 @@ public static class Art
     {
         public Sprite Idle, Jump, Fall;
         public Sprite[] Run;
+    }
+
+    static Sprite Block(Color32 colour, int width = 16, int height = 16)
+    {
+        var texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+        texture.filterMode = FilterMode.Point;
+        var pixels = new Color32[width * height];
+        for (int i = 0; i < pixels.Length; i++) pixels[i] = colour;
+        texture.SetPixels32(pixels);
+        texture.Apply();
+        return Sprite.Create(texture, new Rect(0, 0, width, height),
+            new Vector2(0.5f, 0f), PPU, 0, SpriteMeshType.FullRect);
+    }
+
+    public static PlayerSprites BuildPlayer(int player)
+    {
